3a29838895a33aa88ab717d1612a578cdc43486d
Brando Chiminelli <brando.chiminelli99@gmail.com>
2022-12-10 22:38:12 +0100

    Update snow-feature-pipeline.py

--- src/snow-feature-pipeline.py
+++ src/snow-feature-pipeline.py
@@ -1,24 +1,20 @@
 import os
-#import hopsworks
+import hopsworks
 import pandas as pd
 
-#project = hopsworks.login()
-#fs = project.get_feature_store()
+project = hopsworks.login()
+fs = project.get_feature_store()
 
 snow_df = pd.read_xml("http://dati.meteotrentino.it/service.asmx/tuttiUltimiRilieviNeve")
 
-print(snow_df.head())
+snow_df = snow_df.loc[snow_df['codStaz'] == "31RO"]
+snow_df.drop(['codStaz', 'oraDB','ww', 'n', 'ta', 'tmin', 'tmax', 'hn', 'fi', 't10', 't30', 'pr', 'cs', 's', 'b'], axis='columns', inplace=True)
+snow_df.dropna(inplace=True)
+print(snow_df)
 
-snow_df['codStaz'] = "31RO"
-print(snow_df.head())
-snow_df.drop(['oraDB','Name', 'SibSp', 'Parch', 'Ticket', 'Cabin', 'Embarked'], axis='columns', inplace=True)
-#titanic_df.Sex.replace({'male':1, 'female':0}, inplace=True)
-#titanic_df.rename(columns={"Pclass": "pclass", "Sex": "sex", "Fare": "fare", "Age": "age"})
-
-
-#titanic_fg = fs.get_or_create_feature_group(
-#    name="titanic_modal",
-#    version=1,
-#    primary_key=["pclass","fare","sex","age"], 
-#    description="Titanic survival dataset")
-#titanic_fg.insert(titanic_df, write_options={"wait_for_job" : False})
+snow_fg = fs.get_or_create_feature_group(
+    name="snow_conditions",
+    version=1,
+    primary_key=["dataMis"], 
+    description="Snow conditions dataset")
+snow_fg.insert(snow_df, write_options={"wait_for_job" : False})
