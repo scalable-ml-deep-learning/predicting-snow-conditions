401d7c5ba258358d1a7598f50d7ea9debdc804b3
Brando Chiminelli <brando.chiminelli99@gmail.com>
2022-12-11 16:38:06 +0100

    Update weather-feature-pipeline.py

--- src/weather-feature-pipeline.py
+++ src/weather-feature-pipeline.py
@@ -1,15 +1,11 @@
 import os
-import hopsworks
 import pandas as pd
+import matplotlib.pyplot as plt
 
-#project = hopsworks.login(project="finetune")
-#fs = project.get_feature_store()
-
-weather_df = pd.read_json("https://api.open-meteo.com/v1/forecast?latitude=46.2979&longitude=11.7871&hourly=temperature_2m,snow_depth,weathercode,visibility&models=best_match&current_weather=true&timezone=auto&start_date=2022-12-04&end_date=2022-12-10")
+weather_df = pd.read_json("https://api.open-meteo.com/v1/forecast?latitude=46.2979&longitude=11.7871&hourly=temperature_2m,snow_depth,weathercode,visibility&models=best_match&current_weather=true&timezone=auto&start_date=2022-12-04&end_date=2022-12-11")
 
 weather_df.drop(['latitude', 'longitude','hourly_units', 'generationtime_ms', 'utc_offset_seconds', 'timezone', 'timezone_abbreviation', 'elevation', 'current_weather'], axis='columns', inplace=True)
 weather_df.dropna(inplace=True)
-#print(weather_df.keys)
 
 weather_df = weather_df['hourly']
 for key in weather_df.keys():
@@ -18,15 +14,43 @@ for key in weather_df.keys():
         df_.append(weather_df[key][i])
     weather_df[key] = df_
 
-#weathercode_df = weather_df['weathercode']
-#time_df = weather_df['time']
+print(weather_df)
 
+snow_df = pd.read_xml("http://dati.meteotrentino.it/service.asmx/tuttiUltimiRilieviNeve")
+
+snow_df = snow_df.loc[snow_df['codStaz'] == "31RO"] # Select Passo Rolle
+snow_df.drop(['codStaz', 'oraDB', 'vq1', 'vq2', 'n', 'tmin', 'tmax', 'hn', 'fi', 't10', 't30', 'pr', 'cs', 's', 'b'], axis='columns', inplace=True)
+snow_df.dropna(inplace=True)
+
+snow_df = snow_df.iloc[::-1]
+print(snow_df)
+
+visibility_df = weather_df['visibility']
+for i in range(len(visibility_df)):
+    if visibility_df[i] in range(1000):
+        visibility_df[i] = 1
+    if visibility_df[i] in range(1001, 4000):
+        visibility_df[i] = 2
+    if visibility_df[i] in range(4001, 10000):
+        visibility_df[i] = 3
+    if visibility_df[i] > 10000:
+        visibility_df[i] = 4
+print(visibility_df)
+weather_df['visibility'] = visibility_df
+
+snow_depth_df = weather_df['snow_depth']
+for i in range(len(snow_depth_df)):
+    snow_depth_df[i]*= 100
+print(snow_depth_df)
+weather_df['snow_depth'] = snow_depth_df
 
 print(weather_df)
 
-#snow_fg = fs.get_or_create_feature_group(
-#    name="snow_conditions",
-#    version=1,
-#    primary_key=["dataMis"], 
-#    description="Snow conditions dataset")
-#snow_fg.insert(snow_df, write_options={"wait_for_job" : False})
+x_index = list(range(0,len(weather_df['snow_depth'])))
+print(x_index)
+print(weather_df['temperature_2m'])
+print(snow_df['ta'].iloc[0:])
+plt.plot(x_index, weather_df['weathercode'], label='first')
+plt.plot(x_index, snow_df['ww'].iloc[0:], label='second')
+plt.legend()
+plt.show()
