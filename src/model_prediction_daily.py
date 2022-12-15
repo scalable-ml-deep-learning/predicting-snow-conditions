5d6a5fcc283b4de50c01492835b0bee8807c1966
Brando Chiminelli <brando.chiminelli99@gmail.com>
2022-12-15 11:02:56 +0100

    Create model_prediction_daily.py

---
+++ src/model_prediction_daily.py
@@ -0,0 +1,44 @@
+import os
+import modal
+
+LOCAL=True
+
+if LOCAL == False:
+   stub = modal.Stub("prediction_daily")
+   image = modal.Image.debian_slim().pip_install(["hopsworks==3.0.4", "lxml"]) 
+
+   @stub.function(image=image, schedule=modal.Period(days=1), secret=modal.Secret.from_name("SNOW_API_KEY"))
+   def f():
+       g()
+
+def g():
+    import hopsworks
+    import pandas as pd
+
+    project = hopsworks.login(project="finetune")
+
+
+
+
+
+
+    
+    fs = project.get_feature_store()
+
+    snow_df = pd.read_xml("http://dati.meteotrentino.it/service.asmx/tuttiUltimiRilieviNeve")
+    snow_df = snow_df.loc[snow_df['codStaz'] == "31RO"]
+    snow_df.drop(['codStaz', 'oraDB','ww', 'v', 'vq1', 'vq2', 'n', 'ta', 'tmin', 'tmax', 'hn', 'fi', 't10', 't30', 'pr', 'cs', 's', 'b'], axis='columns', inplace=True)
+    snow_df.dropna(inplace=True)
+    snow_df = snow_df.iloc[:1]
+    #print(snow_df)
+
+    snow_fg = fs.get_feature_group(name="snow_data",version=1)
+    snow_fg.insert(snow_df, write_options={"wait_for_job" : False})
+
+if __name__ == "__main__":
+    if LOCAL == True :
+        g()
+    else:
+        stub.deploy("prediction_daily")
+        with stub.run():
+            f()
