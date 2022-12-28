d39f9aaf9a0c74eefdfada731a7be66195b7ec9d
boyscout99 <tommaso.praturlon@gmail.com>
2022-12-28 12:48:15 +0100

    Fancy plot for app completed

--- src/new_model_prediction_daily.py
+++ src/new_model_prediction_daily.py
@@ -2,8 +2,10 @@ import os
 import modal
 from PIL import Image
 import requests
+import datetime
 import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
+from matplotlib.pyplot import figure
 import numpy as np
 
 
@@ -37,12 +39,20 @@ def emoji_selection(snow_level):
 def plot_snow_prediction(pred_snow):
     '''
     Build fancy histogram for snow levels.
+    Format dates and plot bar chart.
     '''
+    # format dates
+    old_format = '%Y-%m-%d'
+    #new_format = '%a %d, %b'
+    new_format = '%d %b'
     for elem in pred_snow['time']:
-        print("Date: ", elem)
-        elem.strftime("%a %d, %b")
-        print("Format: ", elem)
-        
+        #print("Date: ", elem)
+        new_elem = datetime.datetime.strptime(elem, old_format)
+        new_elem = new_elem.strftime(new_format)
+        #print("Format: ", new_elem)
+        pred_snow['time'].replace(to_replace=elem, value=new_elem, inplace=True)
+
+    # plot bar chart
     plt.bar(
         pred_snow['time'], 
         pred_snow['snow_level_prediction'], 
@@ -51,6 +61,8 @@ def plot_snow_prediction(pred_snow):
     plt.ylabel("Centimeters of snow") 
     plt.title("Snow level forecast for Passo Rolle (TN), Italy")
     plt.yticks(np.arange(0, 101, 10))
+    plt.xticks(rotation = 45) # Rotates X-Axis Ticks by 45-degrees
+    #plt.show()
     plt.savefig('./images/img_pred/plot.png')
     
     return
@@ -60,20 +72,20 @@ def build_pictures_for_app(project, pred_snow):
     Create a plot and six emojis to store in Hopsworks
     for later use in the Huggingface app
     '''
-    print("Length: ", len(pred_snow))
+    dataset_api = project.get_dataset_api()
     for index in range(1,len(pred_snow)+1):
         snow = pred_snow['snow_level_prediction'][index]
-        print("Snow level: ", snow)
+        # print("Snow level: ", snow)
         # select emoji
         emoji = emoji_selection(snow)
         img_url = "https://raw.githubusercontent.com/scalable-ml-deep-learning/predicting-snow-conditions/feature-tommaso/src/images/" + emoji + ".png"
         img = Image.open(requests.get(img_url, stream=True).raw)
         img.save("./images/img_pred/"+str(index)+".png")
         # upload emoji to correspondent index
-        dataset_api = project.get_dataset_api()
         dataset_api.upload("./images/img_pred/"+str(index)+".png", "Resources/img_prediction", overwrite=True)
     
     plot_snow_prediction(pred_snow)
+    dataset_api.upload("./images/img_pred/plot.png", "Resources/img_prediction", overwrite=True)
     print("Uploaded pictures.")
     
     return
@@ -95,7 +107,7 @@ def g():
     fs = project.get_feature_store()
     
     mr = project.get_model_registry()
-    model = mr.get_model("snow_model", version=1)
+    #model = mr.get_model("snow_model", version=1)
     # get best model based on custom metrics
     model = mr.get_best_model("snow_model",
                                EVALUATION_METRIC,
@@ -132,8 +144,8 @@ def g():
     snow_predictions_fg.insert(pred_df, write_options={"wait_for_job" : False})
     
     # create pictures for the app in Huggingface
-    #build_pictures_for_app(project, pred_df)
-    plot_snow_prediction(pred_df)
+    build_pictures_for_app(project, pred_df)
+    #plot_snow_prediction(pred_df)
     
     return
 
