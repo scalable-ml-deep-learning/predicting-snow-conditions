b0ffb0b79d201af320e5e295c13c1494f0c0dff3
boyscout99 <tommaso.praturlon@gmail.com>
2022-12-28 23:19:37 +0100

    Completed app with interface and button

--- src/huggingface-app/app.py
+++ src/huggingface-app/app.py
@@ -11,37 +11,18 @@ import pandas as pd
 project = hopsworks.login(project="finetune")
 fs = project.get_feature_store()
 dataset_api = project.get_dataset_api()
-'''
-for day in range(1,7):
-    img = f'Resources/img_prediction/{day}.png'
-    dataset_api.download(img, overwrite=True)
-    
-dataset_api.download("Resources/img_prediction/plot.png", overwrite=True)
-'''
- 
-def reload_images():
-    '''
-    Get a snow depth level and return an image 
-    indicating good level / bad level with emoji
-    '''
-    for day in range(1,7):
-      img = f'Resources/img_prediction/{day}.png'
-      dataset_api.download(img, overwrite=True)
-    
-    dataset_api.download("Resources/img_prediction/plot.png", overwrite=True)
-    print("Clicked")
-    # refresh page
-    return
     
 def show_reloaded_images():
     '''
     Show new images.
     '''
+    # download emoticons
     for day in range(1,7):
       img = f'Resources/img_prediction/{day}.png'
       dataset_api.download(img, overwrite=True)
-    
+    # download snow prediction forecast
     dataset_api.download("Resources/img_prediction/plot.png", overwrite=True)
+    # optput images
     plot_pred = Image.open("plot.png")
     img1 = Image.open("1.png")
     img2 = Image.open("2.png")
@@ -53,47 +34,43 @@ def show_reloaded_images():
   
     return output
 
-# Uncomment for gradio interface locally on the browser
+def show_history():
+    '''
+    Get history of predictions.
+    '''
+    dataset_api.download("Resources/img_prediction/plot_history.png", overwrite=True)
+    plot_hist = Image.open("plot_history.png")
+    
+    return plot_hist
 
 with gr.Blocks() as demo:
-    with gr.Row():
-      plot_pred = gr.Image(label="Predicted snow height").style(height=500) # plotted graph
-      print("Plot_pred:\n", plot_pred)
-    with gr.Row():
-      #input_img1 = gr.Image("1.png", elem_id="Day 1")
-      img1 = gr.Image()
-      img2 = gr.Image()
-      #gr.Label("Today's Predicted Image")
-      img3 = gr.Image()
-      img4 = gr.Image()
-      img5 = gr.Image()
-      img6 = gr.Image()
-    with gr.Row():  
-      btn = gr.Button("New prediction").style(full_width=True)
-      print("Button:\n", btn)
+    with gr.Tabs():
+        with gr.TabItem("Snow prediction"):
+            with gr.Row():  
+                btn = gr.Button("New prediction").style(full_width=True)
+            with gr.Row():
+                plot_pred = gr.Image(label="Predicted snow height").style(height=500) # plotted graph
+            with gr.Row():
+                #input_img1 = gr.Image("1.png", elem_id="Day 1")
+                img1 = gr.Image()
+                img2 = gr.Image()
+                img3 = gr.Image()
+                img4 = gr.Image()
+                img5 = gr.Image()
+                img6 = gr.Image()
+              
+        with gr.TabItem("Accuracy of past 10 days"):
+            with gr.Row():
+                btn2 = gr.Button("Get history").style(full_width=True)
+            with gr.Row():
+                pred_hist = gr.Image(label="Past 10 days of predictions").style(height=500)
       
     btn.click(show_reloaded_images, 
     inputs=None, 
     outputs=[plot_pred, img1, img2, img3, img4, img5, img6])
-    print("again Plot_pred:\n", plot_pred)
-    #demo.load()
+    
+    btn2.click(show_history,
+    inputs=None,
+    outputs=pred_hist)
 
 demo.launch()
-
-# Previous demo interface
-'''      
-demo = gr.Interface(
-    fn=passenger,
-    title="Snow depth level prediction for Passo Rolle (TN), Italy",
-    description="Just having a forecast of the snow is not enough! From today skiers and snowboarders can actually have a forecast of the snow level!",
-    allow_flagging="never",
-    inputs=[
-        gr.inputs.Number(default=1.0, label="Passenger class"),
-        gr.inputs.Number(default=1.0, label="Passenger sex (1:male, 0:female)"),
-        gr.inputs.Number(default=1.0, label="Passenger age"),
-        gr.inputs.Number(default=1.0, label="Passenger fare")
-        ],
-    outputs=gr.Image(type="pil"))
-
-demo.launch()
-'''
